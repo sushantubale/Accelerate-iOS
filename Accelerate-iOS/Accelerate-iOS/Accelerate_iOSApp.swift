9a884d61a536e692ca53d133dc446d8569951420
Sushant Ubale <sushantubale@gmail.com>
2024-08-11 17:13:41 -0500

    First commit

---
+++ Accelerate-iOS/Accelerate-iOS/Accelerate_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accelerate_iOSApp.swift
+//  Accelerate-iOS
+//
+//  Created by Sushant Ubale on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Accelerate_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
